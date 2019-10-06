abe13a87bce4dc2e626deb25de7f435a45d0961f
Jérôme Blondon <jerome@blondon.fr>
2019-10-06 20:10:53 +0200

    Add vagrant file

---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "debian/buster64"
+  config.vm.provider 'virtualbox' do |vb|
+    vb.customize ['modifyvm', :id, '--memory', 8192]
+    vb.customize ['modifyvm', :id, '--cpus', 2]
+  end
+  config.vm.network "forwarded_port", guest: 9200, host: 9200
+  config.vm.network "forwarded_port", guest: 5601, host: 5601
+  config.vm.network "forwarded_port", guest: 8200, host: 8200
+  config.vm.provision "shell", path: "scripts/install-deps.sh", privileged: true
+  config.vm.synced_folder '.', '/vagrant', owner: 'vagrant', group: 'root'
+end
---
+++ scripts/install-deps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+apt-get update
+apt-get install -y \
+    python-dev \
+    python-pip \
+    gcc \
+    make \
+    build-essential \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+if ! [ -x "$(command -v docker)" ]; then
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh get-docker.sh
+  usermod -aG docker vagrant
+fi
+if ! [ -x "$(command -v docker-compose)" ]; then
+  curl -fsSL "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+fi
+
+echo vm.max_map_count=262144 > /etc/sysctl.d/vm_max_map_count.conf
+sysctl --system
+
+grep -qF 'vagrant - nofile 65536' /etc/security/limits.conf || echo 'vagrant - nofile 65536' >> /etc/security/limits.conf
